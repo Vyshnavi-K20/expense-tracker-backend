33f9e4a11be51cafff4df089895ff61398ef030d
Vyshnavi <vyshnavikumaresan2005@gmail.com>
2025-10-06 21:50:00 +0530

    1of the 2 backends deleted

--- models/Expense.js
+++ models/Expense.js
@@ -4,12 +4,9 @@ const expenseSchema = new mongoose.Schema({
   title: { type: String, required: true },
   amount: { type: Number, required: true },
   category: { type: String, required: true },
-  mood: { type: String, default: "🙂" }, // NEW FIELD
+  mood: { type: String, default: "🙂" }, 
   date: {
     type: Date,
-    default: Date.now, // 👈 fallback if not provided
-  },
-});
-
-
+    default: Date.now, 
+  },});
 module.exports = mongoose.model('Expense', expenseSchema);
